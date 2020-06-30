875b51291b9f0c2215918df33798d2311147ef6f
Amit Nayak <amit.nayak7769@gmail.com>
2020-06-30 15:29:59 -0400

    fdas

--- NotEnoughMoneyException.java
+++ NotEnoughMoneyException.java
@@ -1,18 +1,25 @@
 public class NotEnoughMoneyException extends IllegalStateException{
+
 	private double amount;
 	private double balance;
+
 	public NotEnoughMoneyException(double amount, double balance){
-		super("you have do not have enough money to withdraw " + amount + "$");
-		this.amount=amount;
-		this.balance=balance;
+		super("you have do not have enough money to withdraw $" + amount);
+		this.amount = amount;
+		this.balance = balance;
 	}
+
+	public double getMissingAmount(){
+		return (amount - balance);
+	}
+
 	public double getAmount(){
 		return amount;
 	} 
+
 	public double getBalance(){
 		return balance;
 	}
-	public double getMissingAmount(){
-		return (amount-balance);
-	}   
+
+	   
 }
